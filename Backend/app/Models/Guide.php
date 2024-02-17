fea4367813abe0a2b8ee3bcc3bcaec5d0142a18e
Nafisa7503 <145420919+Nafisa7503@users.noreply.github.com>
2024-02-17 22:38:25 +0600

    Table created
    Admin,Guide,User attributes added.

--- Backend/app/Models/Guide.php
+++ Backend/app/Models/Guide.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Guide extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'name',
+        'email',
+        'year_of_exp',
+        'contact',
+    ];
+
 }
